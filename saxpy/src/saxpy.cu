199bc68f4ab0d6397d1bc213e39c6f2bc7b29dcd
admin <admin@admins-MacBook-Pro.local>
2019-02-25 14:27:09 +0200

    device stats

--- saxpy/src/saxpy.cu
+++ saxpy/src/saxpy.cu
@@ -12,7 +12,25 @@ __global__ void saxpy(int n, float a, float* x, float* y) {
 
 
 int main(int argc, char** argv) {
-	
+
+        int nDevices; 
+        cudaGetDeviceCount(&nDevices);  
+
+        printf("Number of GPU devices %d\n", nDevices);
+
+        for (int i = 0; i < nDevices; i++) {
+       
+            cudaDeviceProp prop;
+            cudaGetDeviceProperties(&prop, i);
+           
+            printf("Device Number: %d\n", i); 
+            printf("  Device Name: %s\n", prop.name);
+            printf("  Memory Clock Rate (Khz): %d\n", prop.memoryClockRate);
+            printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
+            printf("  Peak Memory Bandwidth (GB/s): %f\n",
+                    2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+        }	
+
 	int N = 1<<20; // same as 2^20 or around 1 million. 	
         float *x, *y, *d_x, *d_y;
         x = (float*)malloc(N*sizeof(float));
