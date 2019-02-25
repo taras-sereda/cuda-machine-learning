64de12c916c1d037f8538682713334a9bacb2333
Taras Sereda <taras.sereda@ring.com>
2019-02-25 12:02:39 +0000

    saxpy host part

--- saxpy/src/saxpy.cu
+++ saxpy/src/saxpy.cu
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <cuda_runtime.h>
 
-__global__ void saxpy(int n, float a, float * restrict x, float * b) {
+__global__ void saxpy(int n, float a, float* x, float* y) {
 
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
         if (i < n) y[i] = a * x[i] + y[i];
@@ -13,10 +14,21 @@ __global__ void saxpy(int n, float a, float * restrict x, float * b) {
 int main(int argc, char** argv) {
 	
 	int N = 1<<20; // same as 2^20 or around 1 million. 	
-	cudaMemcpy(d_x, x, N, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_y, y, N, cudaMemcpyHostToDevice);
+        float *x, *y, *d_x, *d_y;
+        x = (float*)malloc(N*sizeof(float));
+        y = (float*)malloc(N*sizeof(float));
+        
+        cudaMalloc(&d_x, N*sizeof(float));
+        cudaMalloc(&d_y, N*sizeof(float));
+       
+        printf("size of float %lu\n", sizeof(float)); 
+        sleep(3);
+        printf("sleeping a bit.\n"); 
 
-	saxpy<<4096, 256>>(N, 2.0, d_x, d_y);
+	cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
 
-	cudaMemcpy(y, d_y, N, cudaMemcpyDeviceToHost);
+	saxpy<<<4096, 256>>>(N, 2.0, d_x, d_y);
+
+	cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
 }
