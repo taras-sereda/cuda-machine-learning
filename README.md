a51fceb13327d3703a683790536246f12dbb32e3
taras <taras.y.sereda@gmail.com>
2021-05-07 21:55:21 +0300

    max threads per block and more device properties

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
+#### Quering device properties.
+
+`cudaDeviceGetAttribute   `argued being [faster](https://developer.nvidia.com/blog/cuda-pro-tip-the-fast-way-to-query-device-properties/]) , though it's  too verbose
+
+```
+    int maxBlockDimX;
+    int maxBlockDimY;
+    int maxBlockDimZ;
+    cudaDeviceGetAttribute(&maxBlockDimX, cudaDevAttrMaxBlockDimX, devIdx);
+    cudaDeviceGetAttribute(&maxBlockDimY, cudaDevAttrMaxBlockDimY, devIdx);
+    cudaDeviceGetAttribute(&maxBlockDimZ, cudaDevAttrMaxBlockDimZ, devIdx);
+    printf("  %d %d %d \n", maxBlockDimX, maxBlockDimY, maxBlockDimZ);
+```
+
+vs. `cudaGetDeviceProperties`
+
+```
+		cudaDeviceProp prop;
+		cudaGetDeviceProperties(&prop, devIdx);
+		printf("  Max Threads Dim: %d %d %d\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+```
+
+
+
+- [reference of CUDA device properties](https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__DEVICE.html)
+
+- [Hemi](https://github.com/harrism/hemi) - library for writting reusable CPU and GPU code. single kernel function executable on both device types. More in this [blog post](https://developer.nvidia.com/blog/simple-portable-parallel-c-hemi-2/).
+
+
+
 1. Compile CUDA kernels
 ```
 nvcc device_info.cu -o device_info
--- device_info.cu
+++ device_info.cu
@@ -5,10 +5,13 @@
 #define ANSI_COLOR_RESET "\x1b[0m"
 #define ANSI_COLOR_CYAN  "\x1B[36m"
 
-int main() {
+int main() {  
   int nDevices;
 
-  cudaGetDeviceCount(&nDevices);
+  cudaError_t err = cudaGetDeviceCount(&nDevices);
+  printf("%s\n", cudaGetErrorString(err));
+  if (err != cudaSuccess) printf("%s\n", cudaGetErrorString(err));
+
   for (int devIdx = 0; devIdx < nDevices; devIdx++) {
     cudaDeviceProp prop;
     cudaGetDeviceProperties(&prop, devIdx);
@@ -17,31 +20,15 @@ int main() {
     printf("  Device compute capability: %d.%d\n", prop.major, prop.minor);
     printf("  LUID: %s\n", prop.luid);
     printf("  CanMapHostMemory: %d\n", prop.canMapHostMemory);
-    printf("  Clock Rate (KHz): %d\n",
-           prop.clockRate);
-    printf("  Memory Clock Rate (KHz): %d\n",
-           prop.memoryClockRate);
-    printf("  Memory Bus Width (bits): %d\n",
-           prop.memoryBusWidth);
-    printf("  Peak Memory Bandwidth (GB/s): %f\n",
-           2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+    printf("  Clock Rate (KHz): %d\n", prop.clockRate);
+    printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
+    printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
+    printf("  Peak Memory Bandwidth (GB/s): %f\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
 
-    // https://developer.nvidia.com/blog/cuda-pro-tip-the-fast-way-to-query-device-properties/
-    printf("  %s%s%s \n", ANSI_COLOR_CYAN, "Querying CUDA Attributes via cudaDeviceGetAttribute", ANSI_COLOR_RESET);
-
-    int memoryClockRate;
-    cudaDeviceGetAttribute(&memoryClockRate, cudaDevAttrMemoryClockRate, devIdx);
-    printf("  Memory Clock Rate (KHz): %d\n", memoryClockRate);
-    //int maxThreadsPerBlock;
-    int maxThreadsDim [3];
-    int maxGridSize [3];
-    //cudaDeviceGetAttribute(&maxThreadsPerBlock, cudaDevAttrMaxThreadsPerBlock, devIdx);
-    //cudaDeviceGetAttribute(&maxThreadsDim, cudaDevAttrMaxThreadsDim, devIdx);
-    //cudaDeviceGetAttribute(&maxGridSize, cudaDevAttrMaxGridSize, devIdx);
     printf("  Max Threads Per Block: %d\n", prop.maxThreadsPerBlock);
-    // printf("  Max Threads Dim: %d\n", &prop.maxThreadsDim[0]);
-    // printf("  Max Grid Size: %d\n", &prop.maxGridSize[0]);
-    std::cout << "  Max Thread Dim x,y,z " << prop.maxThreadsDim[0] << " " << prop.maxThreadsDim[1] << " " << prop.maxThreadsDim[2] << std::endl;
-    std::cout << "  Max Grid Size x,y,z " << prop.maxGridSize[0] << " " << prop.maxGridSize[1] << " " << prop.maxGridSize[2] << std::endl;
+    printf("  %sMax Threads Dim: %d %d %d%s\n", ANSI_COLOR_CYAN, prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2], ANSI_COLOR_RESET);
+    printf("  Max Grid Size: %d %d %d\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+    printf("  Multiprocessor Count: %d\n", prop.multiProcessorCount);
+
   }
 }
