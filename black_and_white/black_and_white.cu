95b6b3157366d5b5fb1608da1f40847c9fdd7a03
Taras Sereda <taras.y.sereda@proton.me>
2024-08-06 18:38:14 +0300

    save grayscale func

--- black_and_white/black_and_white.cu
+++ black_and_white/black_and_white.cu
@@ -1,9 +1,56 @@
+// build
+// nvcc black_and_white.cu -o black_and_white -ljpeg
+
+// launch
+// black_and_white <img_path.jpg>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <jpeglib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+void save_gray_scale_image(const char *filename, unsigned char *imageBuffer, int width, int height)
+{
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    FILE *outfile;
+    JSAMPROW row_pointer[1];
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+
+    if ((outfile = fopen(filename, "wb")) == NULL)
+    {
+        fprintf(stderr, "Can't open file %s\n", filename);
+        jpeg_destroy_compress(&cinfo);
+        return;
+    }
+    jpeg_stdio_dest(&cinfo, outfile);
+
+    cinfo.image_width = width;
+    cinfo.image_height = height;
+    cinfo.input_components = 1;
+    cinfo.in_color_space = JCS_GRAYSCALE;
+
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, 75, TRUE);
+
+    jpeg_start_compress(&cinfo, TRUE);
+
+    while (cinfo.next_scanline < cinfo.image_height)
+    {
+        row_pointer[0] = &imageBuffer[cinfo.next_scanline * width];
+        jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+
+    jpeg_finish_compress(&cinfo);
+    fclose(outfile);
+    jpeg_destroy_compress(&cinfo);
+}
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
         return 1;
     }
@@ -19,7 +66,8 @@ int main(int argc, char *argv[]) {
 
     // Step 2: Specify the source of the compressed data (e.g., a file)
     FILE *infile = fopen(filename, "rb");
-    if (infile == NULL) {
+    if (infile == NULL)
+    {
         fprintf(stderr, "Cannot open %s\n", filename);
         return 1;
     }
@@ -40,7 +88,8 @@ int main(int argc, char *argv[]) {
     printf("width: %d, height: %d, componets: %d\n", cinfo.output_width, cinfo.output_height, cinfo.output_components);
     unsigned char *buffer = (unsigned char *)malloc(row_stride * cinfo.output_height);
 
-    while (cinfo.output_scanline < cinfo.output_height) {
+    while (cinfo.output_scanline < cinfo.output_height)
+    {
         unsigned char *row_pointer = buffer + cinfo.output_scanline * row_stride;
         jpeg_read_scanlines(&cinfo, &row_pointer, 1);
     }
@@ -53,17 +102,30 @@ int main(int argc, char *argv[]) {
 
     fclose(infile);
 
-    // Now you have the image data in the buffer
-    // You can use it as needed in your application
-
     // Example: Print the first 10 bytes
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         printf("%02x ", buffer[i]);
     }
     printf("\n");
 
     // Free the buffer when done
     free(buffer);
+
+    int width = 640;
+    int height = 480;
+    unsigned char *imageBuffer = (unsigned char *)malloc(width * height * sizeof(unsigned char));
+    if (imageBuffer == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed");
+        return 1;
+    }
+    for (int i = 0; i < width * height; i++)
+    {
+        imageBuffer[i] = 128;
+    }
+    save_gray_scale_image("out.jpg", imageBuffer, width, height);
+    free(imageBuffer);
 
     return 0;
 }
