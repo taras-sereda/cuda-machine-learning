afec5c04ecb433ca111a41ba5c389c574724923a
Taras Sereda <taras.y.sereda@proton.me>
2024-08-07 19:14:14 +0300

    image blur kernel

---
+++ blur/image_blur.cu
@@ -0,0 +1,211 @@
+// build
+// nvcc image_blur.cu -o image_blur -ljpeg
+
+// execute
+// image_blur <img_path.jpg>
+
+// note: works for b&w image at the moment, striding in channels is necessary to support arbitrary image bluring.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <jpeglib.h>
+#include <unistd.h>
+
+int const N_CHANNELS = 1;
+
+__global__ void blur_kernel(unsigned char *im_out, unsigned char *im_in, int width, int height, int kernel_size)
+{
+    int col_idx = blockDim.x * blockIdx.x + threadIdx.x;
+    int row_idx = blockDim.y * blockIdx.y + threadIdx.y;
+
+    if (col_idx < width && row_idx < height)
+    {
+        int pix_val = 0;
+        int n_pixels = 0;
+        for (int i = -kernel_size; i < kernel_size + 1; i++)
+        {
+            for (int j = -kernel_size; j < kernel_size + 1; j++)
+            {
+                int cur_row = row_idx + i;
+                int cur_col = col_idx + j;
+
+                if (cur_row < 0 || cur_col < 0 || cur_row == height || cur_col == width)
+                    continue;
+
+                pix_val += im_in[cur_row * width + cur_col];
+                n_pixels += 1;
+            }
+        }
+        im_out[row_idx * width + col_idx] = (unsigned char)(pix_val / n_pixels);
+
+    }
+}
+
+void save_image(const char *filename, unsigned char *image_buffer, int width, int height, J_COLOR_SPACE color_space, int n_channel)
+{
+    // More examples
+    // lib's github: https://github.com/LuaDist/libjpeg/blob/master/example.c
+    // blog: https://www.tspi.at/2020/03/20/libjpegexample.html
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    FILE *outfile;
+    JSAMPROW row_pointer[1];
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+
+    if ((outfile = fopen(filename, "wb")) == NULL)
+    {
+        fprintf(stderr, "Can't open file %s\n", filename);
+        jpeg_destroy_compress(&cinfo);
+        exit(1);
+    }
+    jpeg_stdio_dest(&cinfo, outfile);
+
+    cinfo.image_width = width;
+    cinfo.image_height = height;
+    cinfo.input_components = n_channel;
+    cinfo.in_color_space = color_space;
+
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, 100, TRUE);
+
+    jpeg_start_compress(&cinfo, TRUE);
+
+    // Inefficient, requires fliping and backward reading. Though still ok for my needs.
+    // unsigned char *flipped_row = (unsigned char *)malloc(width * sizeof(unsigned char));
+    // for (int y = height - 1; y >= 0; y--)
+    // {
+    //     // Reverse the order of pixels in each row
+    //     for (int x = 0; x < width; x++)
+    //     {
+    //         flipped_row[x] = image_buffer[y * width + (width - 1 - x)];
+    //     }
+    //     row_pointer[0] = flipped_row;
+    //     jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    // }
+    // should be way faster, byt saved images are rotated.
+    int row_stride = width * n_channel;
+    while (cinfo.next_scanline < cinfo.image_height) {
+        /* jpeg_write_scanlines expects an array of pointers to scanlines.
+        * Here the array is only one element long, but you could pass
+        * more than one scanline at a time if that's more convenient.
+        */
+        row_pointer[0] = & image_buffer[cinfo.next_scanline * row_stride];
+        (void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+    jpeg_finish_compress(&cinfo);
+    fclose(outfile);
+    jpeg_destroy_compress(&cinfo);
+}
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    char *filename = argv[1];
+
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_decompress(&cinfo);
+
+    FILE *infile = fopen(filename, "rb");
+    if (infile == NULL)
+    {
+        fprintf(stderr, "Cannot open %s\n", filename);
+        return 1;
+    }
+
+    jpeg_stdio_src(&cinfo, infile);
+    jpeg_read_header(&cinfo, TRUE);
+    jpeg_start_decompress(&cinfo);
+
+    int row_stride = cinfo.output_width * cinfo.output_components;
+    printf("width: %d, height: %d, componets: %d\n", cinfo.output_width, cinfo.output_height, cinfo.output_components);
+    unsigned char *rgb_buffer = (unsigned char *)malloc(row_stride * cinfo.output_height);
+
+    while (cinfo.output_scanline < cinfo.output_height)
+    {
+        unsigned char *row_pointer = rgb_buffer + cinfo.output_scanline * row_stride;
+        jpeg_read_scanlines(&cinfo, &row_pointer, 1);
+    }
+    jpeg_finish_decompress(&cinfo);
+    jpeg_destroy_decompress(&cinfo);
+
+    fclose(infile);
+
+    // Example: Print the first 10 bytes
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%02x ", rgb_buffer[i]);
+    // }
+    // printf("\n");
+
+    int const width = cinfo.output_width;
+    int const height = cinfo.output_height;
+    size_t gray_size = width * height * sizeof(unsigned char);
+    size_t rgb_size = gray_size * N_CHANNELS;
+    unsigned char *gray_buffer = (unsigned char *)malloc(width * height * sizeof(unsigned char));
+    if (gray_buffer == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed");
+        return 1;
+    }
+    // for (int i = 0; i < width * height; i++)
+    // {
+    //     imageBuffer[i] = 128;
+    // }
+
+    unsigned char *im_in_d, *im_out_d;
+
+    // Allocate memory on device
+    cudaError_t alloc_err_im_in = cudaMalloc(&im_in_d, rgb_size);
+    cudaError_t alloc_err_im_out = cudaMalloc(&im_out_d, gray_size);
+
+    // Copy data from host to device
+    cudaMemcpy(im_in_d, rgb_buffer, rgb_size, cudaMemcpyHostToDevice);
+
+    dim3 dimGrid(ceil(width / 16.0), ceil(height / 16.0), 1);
+    dim3 dimBlock(16, 16, 1);
+
+    blur_kernel<<<dimGrid, dimBlock>>>(im_out_d, im_in_d, width, height, 10);
+
+    // Copy data from device to host
+    cudaMemcpy(gray_buffer, im_out_d, gray_size, cudaMemcpyDeviceToHost);
+
+    // Free device buffers
+    cudaFree(im_in_d);
+    cudaFree(im_out_d);
+
+    // save_image("out.jpg", rgb_buffer, width, height, JCS_RGB, 3);
+    save_image("out_blur.jpg", gray_buffer, width, height, JCS_GRAYSCALE, 1);
+
+    // // Example: Print the first 10 bytes
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%d ", rgb_buffer[i]);
+    // }
+    // printf("\n");
+
+    // // Example: Print the first 10 bytes
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%d ", gray_buffer[i]);
+    // }
+    // printf("\n");
+
+    // Free host buffers
+    free(gray_buffer);
+    free(rgb_buffer);
+
+    // sleep(10);
+
+    return 0;
+}
