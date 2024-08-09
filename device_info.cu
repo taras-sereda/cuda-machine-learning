6c616a70b52ebf7c395797049c3d88bb68a510ed
Taras Sereda <taras.y.sereda@proton.me>
2024-08-10 02:45:02 +0300

    more properties togather with device UUID

--- device_info.cu
+++ device_info.cu
@@ -1,37 +1,51 @@
 #include <iostream>
-#include <stdio.h> 
-
+#include <stdio.h>
 
 #define ANSI_COLOR_RESET "\x1b[0m"
-#define ANSI_COLOR_CYAN  "\x1B[36m"
+#define ANSI_COLOR_CYAN "\x1B[36m"
 
-int main() {  
+int main()
+{
   int nDevices;
 
   cudaError_t err = cudaGetDeviceCount(&nDevices);
   printf("%s\n", cudaGetErrorString(err));
-  if (err != cudaSuccess) printf("%s\n", cudaGetErrorString(err));
+  if (err != cudaSuccess)
+    printf("%s\n", cudaGetErrorString(err));
 
-  for (int devIdx = 0; devIdx < nDevices; devIdx++) {
+  for (int devIdx = 0; devIdx < nDevices; devIdx++)
+  {
     cudaDeviceProp prop;
     cudaGetDeviceProperties(&prop, devIdx);
     printf("Device Number: %d\n", devIdx);
     printf("  Device name: %s\n", prop.name);
     printf("  Device compute capability: %d.%d\n", prop.major, prop.minor);
-    printf("  LUID: %s\n", prop.luid);
-    printf("  CanMapHostMemory: %d\n", prop.canMapHostMemory);
-    printf("  Clock Rate (KHz): %d\n", prop.clockRate);
-    printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
-    printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
-    printf("  Peak Memory Bandwidth (GB/s): %f\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
 
-    printf("  Max Threads Per Block: %d\n", prop.maxThreadsPerBlock);
-    printf("  %sMax Threads Dim: %d %d %d%s\n", ANSI_COLOR_CYAN, prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2], ANSI_COLOR_RESET);
-    printf("  Max Grid Size: %d %d %d\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
-    printf("  Multiprocessor Count, aka SMs: %d\n", prop.multiProcessorCount);
-    printf("  Registers per Block: %d (can be less then per SM!)\n", prop.regsPerBlock);
+    // Print the UUID
+    printf("  UUID: ");
+    for (int i = 0; i < 16; ++i)
+    {
+      printf("%02x", (unsigned char)prop.uuid.bytes[i]);
+      if (i % 4 == 3 && i != 15)
+        printf("-");
+    }
+    printf("\n");
+
+    printf("  LUID: %s(undefined on non-Windows)\n", prop.luid);
+    printf("  CanMapHostMemory: %d\n", prop.canMapHostMemory);
+    printf("  Clock rate (KHz): %d\n", prop.clockRate);
+    printf("  Memory clock rate (KHz): %d\n", prop.memoryClockRate);
+    printf("  Memory bus width (bits): %d\n", prop.memoryBusWidth);
+    printf("  Peak memory bandwidth (GB/s): %f\n", 2.0 * prop.memoryClockRate * (prop.memoryBusWidth / 8) / 1.0e6);
+
+    printf("  Multiprocessor count, aka SMs: %d\n", prop.multiProcessorCount);
+    printf("  Max threads per SM: %d\n", prop.maxThreadsPerMultiProcessor);
+    printf("  Registers per block: %d (can be less then per SM!)\n", prop.regsPerBlock);
     printf("  Registers per SM: %d\n", prop.regsPerMultiprocessor);
     printf("  Warp Size: %d\n", prop.warpSize);
 
+    printf("  Max grid size: %d %d %d\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+    printf("  Max threads per block: %d\n", prop.maxThreadsPerBlock);
+    printf("  %sMax threads dim: %d %d %d%s\n", ANSI_COLOR_CYAN, prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2], ANSI_COLOR_RESET);
   }
 }
