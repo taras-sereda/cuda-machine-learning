803f49ae61e3cce2b3e4593b07139d3a1acb5e2f
Taras Sereda <taras.y.sereda@proton.me>
2025-02-19 11:17:20 -0800

    formatting and cleanup

--- src/attention/main.cu
+++ src/attention/main.cu
@@ -1,69 +1,60 @@
 #include "../kernel.cu"
 #include "../util.c"
 
-
-#include <stdlib.h>
-#include <stdio.h>
 #include <cmath>
 #include <cuda_runtime.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int a_rows = 4;
-    int a_cols = 8;
-    int b_rows = 8;
-    int b_cols = 2;
+  int a_rows = 4;
+  int a_cols = 8;
+  int b_rows = 8;
+  int b_cols = 2;
 
-    // static_assert(a_cols == b_rows, "Matrix dimensions are not compatible for multiplication");
-    int a_size = a_rows*a_cols * sizeof(float);
-    int b_size = b_rows*b_cols * sizeof(float);
-    int c_size = a_rows*b_cols * sizeof(float);
+  float *A_host, *B_host, *C_host;
+  float *A_device, *B_device, *C_device;
 
-    float *A_host = (float *)malloc(a_size);
-    float *B_host = (float *)malloc(b_size);
-    float *C_host = (float *)malloc(c_size);
+  int a_size = a_rows * a_cols * sizeof(float);
+  int b_size = b_rows * b_cols * sizeof(float);
+  int c_size = a_rows * b_cols * sizeof(float);
 
-    // init host data
-    init(A_host, a_rows*a_cols);
-    init(B_host, b_rows*b_cols);
+  A_host = (float *)malloc(a_size);
+  B_host = (float *)malloc(b_size);
+  C_host = (float *)malloc(c_size);
 
-    printf("A:\n");
-    print_tensor(A_host, a_rows*a_cols);
-    printf("\n");
+  // init host data
+  init(A_host, a_rows * a_cols);
+  init(B_host, b_rows * b_cols);
 
-    printf("B:\n");
-    print_tensor(B_host, b_rows*b_cols);
-    printf("\n");    
+  cudaMalloc((void **)&A_device, a_size);
+  cudaMalloc((void **)&B_device, b_size);
+  cudaMalloc((void **)&C_device, c_size);
 
-    float *A_device;
-    float *B_device;
-    float *C_device;
+  cudaMemcpy(A_device, A_host, a_size, cudaMemcpyHostToDevice);
+  cudaMemcpy(B_device, B_host, b_size, cudaMemcpyHostToDevice);
 
-    cudaMalloc(&A_device, a_size);
-    cudaMalloc(&B_device,  b_size);
-    cudaMalloc(&C_device, c_size);
+  dim3 threadsPerBlock(16, 16);
 
-    cudaMemcpy(A_device, A_host, a_size, cudaMemcpyHostToDevice);
-    cudaMemcpy(B_device, B_host, b_size, cudaMemcpyHostToDevice);
+  dim3 numBlocks((b_rows + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                 (a_cols + threadsPerBlock.y - 1) / threadsPerBlock.y);
 
-    dim3 threadsPerBlock(16, 16);
+  naiveMatMulKernel<<<numBlocks, threadsPerBlock>>>(
+      A_device, B_device, C_device, a_rows, a_cols, b_cols);
 
-    dim3 numBlocks((b_rows + threadsPerBlock.x - 1) / threadsPerBlock.x, (a_cols + threadsPerBlock.y - 1) / threadsPerBlock.y);
+  cudaMemcpy(C_host, C_device, c_size, cudaMemcpyDeviceToHost);
 
-    printf("%d %d \n", numBlocks.x, numBlocks.y);
+  cudaDeviceSynchronize();
 
-    matMulKernel<<<numBlocks, threadsPerBlock>>>(A_device, B_device, C_device, a_rows, a_cols, b_cols);
+  cudaFree(A_device);
+  cudaFree(B_device);
+  cudaFree(C_device);
 
-    cudaMemcpy(C_host, C_device, c_size, cudaMemcpyDeviceToHost);
+  print_tensor(C_host, a_rows * b_cols);
 
-    cudaFree(A_device);
-    cudaFree(B_device);
-    cudaFree(C_device);
+  free(A_host);
+  free(B_host);
+  free(C_host);
 
-    print_tensor(C_host, a_rows*b_cols);
-    free(A_host);
-    free(B_host);
-    free(C_host);
-    
-    return 0;
+  return 0;
 }
