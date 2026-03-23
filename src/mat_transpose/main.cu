3e226ef54e4b2011eb8a9df5c092dd396d69ba8c
taras-sereda <taras.y.sereda@proton.me>
2026-03-22 21:45:38 -0700

    naive mat transpose

--- src/mat_transpose/main.cu
+++ src/mat_transpose/main.cu
@@ -3,68 +3,89 @@
 
 #include <cuda_runtime.h>
 
-#define BLOCK_SIZE 16 
+#define BLOCK_SIZE 16
 
-typedef struct {
+typedef struct
+{
     int width;
     int height;
     float *elements;
 } Matrix;
 
-__global__ void matTransposeKernel(Matrix A, Matrix B) {
+__global__ void matTransposeKernelNaive(Matrix A, Matrix B)
+{
 
     int row_idx = blockIdx.y * BLOCK_SIZE + threadIdx.y;
     int col_idx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
 
+    if (row_idx < A.height && col_idx < A.width)
+    {
+        B.elements[B.width * col_idx + row_idx] =
+            A.elements[A.width * row_idx + col_idx];
+    }
+}
+__global__ void matTransposeKernel(Matrix A, Matrix B)
+{
+
+    int row_idx = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+    int col_idx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
 
     __shared__ float tile[BLOCK_SIZE][BLOCK_SIZE];
-    //Bank conflict resolvement magic.
+    // Bank conflict resolvement magic.
     //__shared__ float tile[BLOCK_SIZE][BLOCK_SIZE+1];
 
-
-    if (row_idx < A.height && col_idx < A.width) {
+    if (row_idx < A.height && col_idx < A.width)
+    {
         tile[threadIdx.y][threadIdx.x] = A.elements[A.width * row_idx + col_idx];
-
     }
     __syncthreads();
 
-    // Transposed tile
+    // Transposed offsets.
     int trans_row = blockIdx.x * BLOCK_SIZE + threadIdx.y;
     int trans_col = blockIdx.y * BLOCK_SIZE + threadIdx.x;
 
-    if (trans_row < B.height && trans_col < B.width) {
-	// Coalesced write
+    // printf("Thread (%d, %d) in block (%d, %d)\n", threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y);
+
+    if (trans_row < B.height && trans_col < B.width)
+    {
+        // Coalesced write to a transposed location.
         B.elements[B.width * trans_row + trans_col] =
-           tile[threadIdx.x][threadIdx.y];
+            tile[threadIdx.x][threadIdx.y];
     }
 }
- 
-void init_matrix(float *elements, int width, int height) {
 
-    for(int i=0; i< height; i++){
-        for(int j=0; j< width; j++){
+void init_matrix(float *elements, int width, int height)
+{
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             int idx = width * i + j;
-            elements[idx] = random()/(float)RAND_MAX;
+            elements[idx] = rand() / (float)RAND_MAX;
         }
     }
 }
 
+void check_res(Matrix A, Matrix B)
+{
 
-void check_res(Matrix A, Matrix B) {
-
-    for(int i=0; i<A.height; i++){
-        for(int j=0; j<A.width; j++){
+    for (int i = 0; i < A.height; i++)
+    {
+        for (int j = 0; j < A.width; j++)
+        {
             int a_idx = A.width * i + j;
             int b_idx = A.height * j + i;
-            printf("Matrix A [%d][%d] = %.4f\n", i, j, A.elements[a_idx]);
-            printf("Matrix B [%d][%d] = %.4f\n", j, i, B.elements[b_idx]);
+            printf("Matrix A [%d][%d] = %.4f Matrix B [%d][%d] = %.4f\n",
+                   i, j, A.elements[a_idx], j, i, B.elements[b_idx]);
         }
     }
 }
 
-int main(int argc, char** argv) {
-    int height = 1024;
-    int width = 2049;
+int main(int argc, char **argv)
+{
+    int height = 8;
+    int width = 17;
     Matrix h_A;
     h_A.height = height;
     h_A.width = width;
@@ -76,7 +97,6 @@ int main(int argc, char** argv) {
     d_A.height = h_A.height;
     d_A.width = h_A.width;
     cudaMalloc(&d_A.elements, A_size);
-
 
     Matrix h_B;
     h_B.height = width;
@@ -91,26 +111,23 @@ int main(int argc, char** argv) {
 
     cudaMemcpy(d_A.elements, h_A.elements, A_size, cudaMemcpyHostToDevice);
 
-
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 dimGrid((h_A.width + BLOCK_SIZE -1)/BLOCK_SIZE, (h_A.height + BLOCK_SIZE -1)/BLOCK_SIZE);
+    dim3 dimGrid((h_A.width + BLOCK_SIZE - 1) / BLOCK_SIZE, (h_A.height + BLOCK_SIZE - 1) / BLOCK_SIZE);
 
     matTransposeKernel<<<dimGrid, dimBlock>>>(d_A, d_B);
     cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess) {
+    if (err != cudaSuccess)
+    {
         printf("Kernel launch failed: %s\n", cudaGetErrorString(err));
     }
     cudaDeviceSynchronize();
     cudaMemcpy(h_B.elements, d_B.elements, B_size, cudaMemcpyDeviceToHost);
 
-    check_res(h_A, h_B);
-
     cudaFree(d_A.elements);
     cudaFree(d_B.elements);
 
+    check_res(h_A, h_B);
+
     free(h_A.elements);
     free(h_B.elements);
-
 }
-
-
